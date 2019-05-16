00972cc631afa78de1535e0e0a8ecdf561b166ed
Akshay Darji <akshay7694@live.com>
2019-05-16 21:46:51 +0530

    Code for XML parsing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # xml-go-demo
+
 An example to encode/decode XML files in go
+
+It uses the `Unmarshal` function in Go's `xml` package
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// Recurlyservers is a struct which contains data of parsed XML
+type Recurlyservers struct {
+	XMLName     xml.Name `xml:"servers"`
+	Version     string   `xml:"version,attr"`
+	Svs         []server `xml:"server"`
+	Description string   `xml:",innerxml"`
+}
+
+type server struct {
+	XMLName    xml.Name `xml:"server"`
+	ServerName string   `xml:"serverName"`
+	ServerIP   string   `xml:"serverIP"`
+}
+
+func main() {
+	file, err := os.Open("servers.xml") // For read access
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+	defer file.Close()
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+	v := Recurlyservers{}
+	err = xml.Unmarshal(data, &v)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	fmt.Println(v)
+}
